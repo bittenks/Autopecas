952b4c6e878550c22f81a0900170831662adef3d
Lucas Henrique Soares Cruz <69327915+Lucaspla636@users.noreply.github.com>
2021-03-12 23:08:01 -0300

    Add files via upload

---
+++ AutoPecas/src/Model/Produto.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author henri
+ */
+public class Produto {
+    
+    private int idProduto;
+    private String nomeProduto;
+    private String descricaoProduto;
+    private int quantidadeProduto ;
+    private float valorProduto ;
+    private String categoriaProduto;
+    private String marcaProduto ;
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getDescricaoProduto() {
+        return descricaoProduto;
+    }
+
+    public void setDescricaoProduto(String descricaoProduto) {
+        this.descricaoProduto = descricaoProduto;
+    }
+
+    public int getQuantidadeProduto() {
+        return quantidadeProduto;
+    }
+
+    public void setQuantidadeProduto(int quantidadeProduto) {
+        this.quantidadeProduto = quantidadeProduto;
+    }
+
+    public float getValorProduto() {
+        return valorProduto;
+    }
+
+    public void setValorProduto(float valorProduto) {
+        this.valorProduto = valorProduto;
+    }
+
+    public String getCategoriaProduto() {
+        return categoriaProduto;
+    }
+
+    public void setCategoriaProduto(String categoriaProduto) {
+        this.categoriaProduto = categoriaProduto;
+    }
+
+    public String getMarcaProduto() {
+        return marcaProduto;
+    }
+
+    public void setMarcaProduto(String marcaProduto) {
+        this.marcaProduto = marcaProduto;
+    }
+
+    
+     public Produto (){
+         
+     }
+             
+             
+    public Produto(int idProduto, String nomeProduto, String descricaoProduto, int quantidadeProduto, float valorProduto, String categoriaProduto, String marcaProduto) {
+        this.idProduto = idProduto;
+        this.nomeProduto = nomeProduto;
+        this.descricaoProduto = descricaoProduto;
+        this.quantidadeProduto = quantidadeProduto;
+        this.valorProduto = valorProduto;
+        this.categoriaProduto = categoriaProduto;
+        this.marcaProduto = marcaProduto;
+    }
+    
+   
+
+  
+  
+    
+    
+    
+}
