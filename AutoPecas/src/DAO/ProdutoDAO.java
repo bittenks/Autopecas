4c2b29113dae931d5c7a1cfe41940d960d932ef2
Lucas Henrique Soares Cruz <69327915+Lucaspla636@users.noreply.github.com>
2021-03-12 23:03:54 -0300

    Add files via upload

---
+++ AutoPecas/src/DAO/ProdutoDAO.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Model.Produto;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author henri
+ */
+public class ProdutoDAO {
+    
+    PreparedStatement pst;
+    String sql;
+    
+    public void salvar(Produto produto) throws SQLException{
+        
+        sql = "insert into produto values (?, ?, ?, ?, ?, ?, ?)";
+        pst = Conexao.getInstance().prepareStatement(sql);
+        pst.setInt(1, 0);
+        pst.setString(2, produto.getNomeProduto());
+        pst.setString(3, produto.getDescricaoProduto());
+        pst.setInt(4, produto.getQuantidadeProduto());
+        pst.setFloat(5, produto.getValorProduto());
+        pst.setString(6, produto.getCategoriaProduto());
+        pst.setString(7, produto.getMarcaProduto());
+       
+        pst.execute();
+        pst.close();
+
+    }
+    
+    public void excluir(Produto produto) throws SQLException{
+        sql = "delete from produto where idProduto=?";
+        pst = Conexao.getInstance().prepareStatement(sql);
+        pst.setInt(1, produto.getIdProduto());
+        pst.execute();
+        pst.close();
+    }
+    
+    public void alterar (Produto produto) throws SQLException{
+        sql = "update produto set nomeproduto=?, descricaoProduto=?, quantidadeProduto=?, valorProduto=?, categoriaProduto=?,"
+                + "marcaProduto=?";
+       pst = Conexao.getInstance().prepareStatement(sql);
+        pst.setInt(1, 0);
+        pst.setString(2, produto.getNomeProduto());
+        pst.setString(3, produto.getDescricaoProduto());
+        pst.setInt(4, produto.getQuantidadeProduto());
+        pst.setFloat(5, produto.getValorProduto());
+        pst.setString(6, produto.getCategoriaProduto());
+        pst.setString(7, produto.getMarcaProduto());
+       
+        pst.execute();
+        pst.close();
+    }
+    
+    public Produto buscaProdutoCodigo(String idProduto) throws SQLException{
+        sql = "select * from produto where idProduto = " + idProduto;
+        pst = Conexao.getInstance().prepareStatement(sql);
+        ResultSet rs = pst.executeQuery();
+        Produto pro = null;
+        while (rs.next()){
+            pro = new Produto(rs.getInt("idProduto"), rs.getString("nomeProduto"), rs.getString("descricaoProduto"),rs.getInt("quantidadeProduto"),
+                    rs.getFloat("valorProduto"), rs.getString("categoriaProduto"), rs.getString("marcaProduto"));
+        }
+        pst.close();
+        return pro;
+    }
+    
+    public List<Produto> ListaProduto() throws SQLException {
+        List<Produto> listaProdutos;
+        listaProdutos= new ArrayList<>();
+        sql = "select * from produto order by nomeProduto";
+        pst = Conexao.getInstance().prepareStatement(sql);
+        ResultSet rs = pst.executeQuery();
+        while (rs.next()){
+            listaProdutos.add(new Produto(rs.getInt("idProduto"), rs.getString("nomeProduto"), rs.getString("descricaoProduto"),rs.getInt("quantidadeProduto"),
+                    rs.getFloat("valorProduto"), rs.getString("categoriaProduto"), rs.getString("marcaProduto")));
+        } 
+        pst.close();
+        return listaProdutos;
+    
+    } 
+}
